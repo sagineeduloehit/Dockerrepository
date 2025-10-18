54a796d2d8c80922e04223d15efa842a47a4c017
BHARGAVSAI558 <bhargavsai954@gmail.com>
2025-10-18 10:38:39 +0530

    hello

--- frontend-reactapp/src/App.jsx
+++ frontend-reactapp/src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import NavBar from './components/NavBar';
+ 
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-      <Navbar />
+      <NavBar/>
       </BrowserRouter>
     </div>
   );
